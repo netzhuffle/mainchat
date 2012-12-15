41672c52da8f343ee9cb80ecf7ef23f07b650976
Jannis Grimm <jannis@gje.ch>
2012-12-15 20:49:29 +0100

    Rename to Open mainChat

--- functions.php-index.php
+++ functions.php-index.php
@@ -830,7 +830,8 @@ function zeige_fuss()
     global $body_tag, $layout_fuss, $layout_include, $layout_parse, $ivw, $layout_bodytag;
     global $f3, $f4, $mainchat_version;
     
-    echo "<DIV align=center>" . $f3 . $mainchat_version . $f4
+    echo "<DIV align=center>" . $f3 . $mainchat_version
+        . "<BR><A HREF=\"https://github.com/netzhuffle/mainchat\">mainChat Open Source</A>" . $f4
         . "</DIV>\n<BR CLEAR=ALL>\n" . "<BR CLEAR=ALL>\n";
     
     if (isset($banner) && strlen($banner) > 0) {
