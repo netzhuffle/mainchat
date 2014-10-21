827af472b03e98966a427eb94440c96931b6f929
maytop <fusion@fusion>
2014-10-21 20:26:47 +0100

    Anpassung Installtool: - $_POST statt register_globals - addslashes hinzugefügt, damit insbesondere auch $noframes und $stylesheet ausgelesen werden können

--- install/install.php
+++ install/install.php
@@ -17,42 +17,42 @@ $configdatei = "../conf/config.php";
 echo "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"8\" border=\"2\" bordercolor=\"#007ABE\" bgcolor=\"#E5E5E5\" style=\"font-family: Arial;\">\n";
 echo "<tr bgcolor=\"#007ABE\"><td><p style=\"font-size:20px;text-align:center;color:White;font-family:Arial;\"><b>Mainchat Installation</b></p></td></tr><tr><td>\n";
 
-switch ($aktion) {
+switch ($_POST['aktion']) {
     case "step_2":
         if (!file_exists($configdatei)) {
             $regexemail = "|^[_a-z0-9-]+(\.[_a-z0-9]+)*@([a-z0-9-]+\.)[a-z]{2,3}$|mi";
             
-            if ($chat['lobby'] == "" || $chat['dbase'] == ""
-                || $chat['host'] == "" || $chat['user'] == ""
-                || $chat['pass'] != $chat['pass2']
-                || ($chat['webmaster'] == ""
-                    || (!preg_match($regexemail, $chat['webmaster'])))
-                || ($chat['hackmail'] == ""
-                    || (!preg_match($regexemail, $chat['hackmail'])))
-                || $chat['chatname'] == "") {
+            if ($_POST['chat']['lobby'] == "" || $_POST['chat']['dbase'] == ""
+                || $_POST['chat']['host'] == "" || $_POST['chat']['user'] == ""
+                || $_POST['chat']['pass'] != $_POST['chat']['pass2']
+                || ($_POST['chat']['webmaster'] == ""
+                    || (!preg_match($regexemail, $_POST['chat']['webmaster'])))
+                || ($_POST['chat']['hackmail'] == ""
+                    || (!preg_match($regexemail, $_POST['chat']['hackmail'])))
+                || $_POST['chat']['chatname'] == "") {
                 echo "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" border=\"0\" align=\"center\">\n"
                     . "<tr bgcolor=\"#007ABE\"><td colspan=\"2\" style=\"font-size:15px; text-align:center;color:White;\"><B>FEHLER</b></td></tr>\n";
-                if ($chat['lobby'] == "")
+                if ($_POST['chat']['lobby'] == "")
                     echo "<tr style=\"color:red;font-weigth:bold;\"><td>Bitte tragen Sie ein Lobby Defaultraum ein!</td></tr>\n";
-                if ($chat['dbase'] == "")
+                if ($_POST['chat']['dbase'] == "")
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Bitte tragen Sie den Datenbanknamen ein!</td></tr>\n";
-                if ($chat['host'] == "")
+                if ($_POST['chat']['host'] == "")
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Bitte tragen Sie den Datenbankserver ein!</td></tr>\n";
-                if ($chat['user'] == "")
+                if ($_POST['chat']['user'] == "")
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Bitte tragen Sie den Datenbankuser ein!</td></tr>\n";
-                if ($chat['pass'] != $chat['pass2'])
+                if ($_POST['chat']['pass'] != $_POST['chat']['pass2'])
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Das Passwort stimmt nicht überein!</td></tr>\n";
-                if ($chat['webmaster'] == "")
+                if ($_POST['chat']['webmaster'] == "")
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Bitte tragen Sie einen Webmaster ein!</td></tr>\n";
-                if ((!preg_match($regexemail, $chat['webmaster']))
-                    && ($chat['webmaster'] != ""))
+                if ((!preg_match($regexemail, $_POST['chat']['webmaster']))
+                    && ($_POST['chat']['webmaster'] != ""))
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Ungültige Email-Adresse Webmaster!</td></tr>\n";
-                if ($chat['hackmail'] == "")
+                if ($_POST['chat']['hackmail'] == "")
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Bitte tragen Sie eine zutändige Person bei Hackversuchen ein!</td></tr>\n";
-                if ((!preg_match($regexemail, $chat['hackmail']))
-                    && ($chat['hackmail'] != ""))
+                if ((!preg_match($regexemail, $_POST['chat']['hackmail']))
+                    && ($_POST['chat']['hackmail'] != ""))
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Ungültige Email-Adresse Hackmail!</td></tr>\n";
-                if ($chat['chatname'] == "")
+                if ($_POST['chat']['chatname'] == "")
                     echo "<tr style=\"color:red; font-weigth:bold;\"><td>Bitte tragen Sie den Chatnamen ein!</td></tr>\n";
                 echo "<tr><td colspan=\"2\"><br><br></td></tr></table>\n";
                 step_1();
@@ -65,8 +65,8 @@ switch ($aktion) {
                         . "<tr><td colspan=\"2\"><br><br></td></tr></table>\n";
                     step_1();
                 } else {
-                    if (!@$connect = mysql_connect($chat['host'],
-                        $chat['user'], $chat['pass'])) {
+                    if (!@$connect = mysql_connect($_POST['chat']['host'],
+                        $_POST['chat']['user'], $_POST['chat']['pass'])) {
                         echo "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" border=\"0\" align=\"center\">\n"
                             . "<tr bgcolor=\"#007ABE\"><td colspan=\"2\" style=\"font-size:15px; text-align:center;color:White;\"><B>FEHLER</b></td></tr>\n"
                             . "<tr style=\"color:red; font-weigth:bold;\"><td>FEHLER: Datenbankverbindung fehlgeschlagen!</td></tr>\n"
@@ -75,9 +75,9 @@ switch ($aktion) {
                         step_1();
                     } else {
                         mysql_set_charset("utf8");
-                        if (!$select = mysql_select_db($chat['dbase'], $connect)) {
+                        if (!$select = mysql_select_db($_POST['chat']['dbase'], $connect)) {
                             if (!$create_db = mysql_query(
-                                "CREATE DATABASE $chat[dbase]", $connect)) {
+                                "CREATE DATABASE ".$_POST['chat']['dbase'], $connect)) {
                                 echo "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" border=\"0\" align=\"center\">\n"
                                     . "<tr bgcolor=\"#007ABE\"><td colspan=\"2\" style=\"font-size:15px; text-align:center;color:White;\"><B>FEHLER</b></td></tr>\n"
                                     . "<tr style=\"color:red; font-weigth:bold;\"><td>FEHLER: Anlegen der Datenbank misslungen!</td></tr>\n"
@@ -85,12 +85,12 @@ switch ($aktion) {
                                 unlink($configdatei);
                                 step_1();
                             } else {
-                                $select = mysql_select_db($chat['dbase'],
+                                $select = mysql_select_db($_POST['chat']['dbase'],
                                     $connect);
-                                step_2($connect, $select, $chat, $fp);
+                            step_2($connect, $select, $_POST['chat'], $fp);
                             }
                             
-                        } else step_2($connect, $select, $chat, $fp);
+                        } else step_2($connect, $select, $_POST['chat'], $fp);
                     }
                 }
             }
